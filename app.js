85ee3a5bfe28a561034f419c02866f3332e28777
Kirill Zhidkov <kirill-info@yandex.ru>
2022-07-29 00:13:55 +0300

    Настройка и установка компонентов

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 4000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
