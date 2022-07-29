8ffe9aaf372a1b37448b79c198a5e24ffb77c66b
Kirill <efes2002@gmail.com>
2022-07-29 16:17:06 +0300

    29-04-2022 01

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+
 
 const { PORT = 4000 } = process.env;
 
@@ -9,6 +12,17 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 });
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login)
+
+app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxLength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
